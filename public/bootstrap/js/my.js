eeab0a1f089fe8298d462adec07ffe411c3c4c6e
Renato Ferreira <renatopsf@icloud.com>
2014-07-12 13:38:26 -0300

    all right

--- public/bootstrap/js/my.js
+++ public/bootstrap/js/my.js
@@ -7,6 +7,22 @@ $(function(){
        $('.bem').delay(1000).fadeIn(1200);
         $('.vindo').delay(2500).fadeIn(1500);
         $('.pecas').delay(4000).fadeIn(2000);
-    });
+         });
+        var contagem = ('.contagem');
+        $(contagem).delay(6000).fadeIn(1000, function(){
+        var count = 2;
+            setInterval(function(){
+            $(contagem).text(count);
+                if (count == 0){
+                    $(contagem).text('GO!');
+                    window.location = 'localhost/projetos/php-oop/pecasonline/home';
+                    return false;
+
+                }
+
+                count --;
+        }, 1000);
+
+});
 
 });
